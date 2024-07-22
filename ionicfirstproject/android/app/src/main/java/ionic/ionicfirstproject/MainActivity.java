20221b1b780453450736ff95066d535600adab6e
jaffarabbas <gamaportal8@gmail.com>
2024-07-22 22:57:51 +0500

    project initialize

---
+++ ionicfirstproject/android/app/src/main/java/ionic/ionicfirstproject/MainActivity.java
@@ -0,0 +1,5 @@
+package ionic.ionicfirstproject;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ ionicfirstproject/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.ionicfirstproject',
+  appName: 'ionicfirstproject',
+  webDir: 'www'
+};
+
+export default config;
